24ae8272f891e7e07a794151a30ee71408e66112
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2017-12-30 01:44:51 +0300

    FDPT-Proxy Implementation.

---
+++ Test.DesignPatterns.UnitTets/Structural Patterns/Proxy/Proxy.Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Test.DesignPatterns.UnitTets.Structural_Patterns.Proxy.Implementation;
+
+namespace Test.DesignPatterns.UnitTets.Structural_Patterns.Proxy
+{
+    [TestFixture]
+    public class Proxy
+    {
+        [Test]
+        public void ProxyTest()
+        {
+            // Create math proxy
+
+            MathProxy proxy = new MathProxy();
+
+            // Do the math
+
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
