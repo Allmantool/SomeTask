02d2b7203103b8946c3e55bb702f0123f305e37d
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2017-12-31 02:31:52 +0300

    FDPT-Composite Implementation.

---
+++ Test.DesignPatterns.UnitTets/Structural Patterns/Composite/Abstraction/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test.DesignPatterns.UnitTets.Structural_Patterns.Composite.Abstraction
+{
+    public abstract class Component
+    {
+        protected readonly string Name;
+
+        protected Component(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Add(Component component) { }
+
+        public virtual void Remove(Component component) { }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
