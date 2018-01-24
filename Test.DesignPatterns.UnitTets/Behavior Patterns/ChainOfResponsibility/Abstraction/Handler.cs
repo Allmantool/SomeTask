3aafb524c0dc187dc14bf156aad0497926bf4ff4
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2018-01-24 08:39:11 +0300

    FDPT-ChainOfRenposibility.

---
+++ Test.DesignPatterns.UnitTets/Behavior Patterns/ChainOfResponsibility/Abstraction/Handler.cs
@@ -0,0 +1,8 @@
+﻿namespace Test.DesignPatterns.UnitTets.Behavior_Patterns.ChainOfResponsibility.Abstraction
+{
+    public abstract class Handler
+    {
+        public Handler Successor { get; set; }
+        public abstract void HandleRequest(int condition);
+    }
+}
---
+++ Test.DesignPatterns.UnitTets/Behavior Patterns/ChainOfResponsibility/ChainOfResponsibility.Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Test.DesignPatterns.UnitTets.Behavior_Patterns.ChainOfResponsibility
+{
+    [TestFixture()]
+    public class ChainOfResponsibility
+    {
+        [Test]
+        public void MainTest()
+        {
+            //Handler h1 = new ConcreteHandler1();
+            //Handler h2 = new ConcreteHandler2();
+            //h1.Successor = h2;
+            //h1.HandleRequest(2);
+        }
+    }
+}
