af877af65cf1aaf3c3c0816a716f6992c156cc8a
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2018-02-05 16:55:02 +0300

    FDPT-State pattern implementation.

---
+++ Test.DesignPatterns.UnitTets/Behavior Patterns/State/State.Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Test.DesignPatterns.UnitTets.Behavior_Patterns.State.Implementation;
+
+namespace Test.DesignPatterns.UnitTets.Behavior_Patterns.State
+{
+    [TestFixture]
+    public class StatePattern
+    {
+        [Test]
+        public void Main()
+        {
+            // Open a new account
+
+            Account account = new Account("Jim Johnson");
+
+            // Apply financial transactions
+
+            account.Deposit(500.0);
+            account.Deposit(300.0);
+            account.Deposit(550.0);
+            account.PayInterest();
+            account.Withdraw(2000.00);
+            account.Withdraw(1100.00);
+        }
+    }
+}
