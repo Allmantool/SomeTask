1a4b2ef5566a61dfcabc64097ca90219d8ccfb9a
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2018-02-16 00:15:53 +0300

    NPM-Selenium

--- HTTPClient/Program.cs
+++ HTTPClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using OpenQA.Selenium;
 
 namespace HTTPClient
 {
@@ -10,33 +11,42 @@ namespace HTTPClient
     {
         public static void Main(string[] args)
         {
-            var httpClientHandler = new HttpClientHandler()
-            {
-                Credentials = new NetworkCredential("296874243", "geneva2788", "npm.by"),
-            };
+            var browser = new OpenQA.Selenium.Chrome.ChromeDriver();
 
-            var client = new HttpClient(httpClientHandler)
-            {
-                BaseAddress = new Uri(@"https://npm.by/"),
-            };
+            browser.Navigate().GoToUrl(@"https://npm.by/");
 
-            //id_departure_station: 36
-            //departure_is_waypoint: 0
-            //id_arrival_station: 63
-            //arrival_is_waypoint: 0
-            //date: 06 - 01 - 2018
+            var element = browser.FindElement(By.Id("reserveDateItem"));
+            element.Click();
 
-            var pairs = new List<KeyValuePair<string, string>>
-            {
-                new KeyValuePair<string, string>( "id_departure_station", "36" ),
-                new KeyValuePair<string, string>( "departure_is_waypoint", "0"  ),
-                new KeyValuePair<string, string> ( "id_arrival_station", "63"  ),
-                new KeyValuePair<string, string> ( "arrival_is_waypoint","0" ),
-                new KeyValuePair<string, string> ( "date", "06-01-2018" )
-            };
-            var content = new FormUrlEncodedContent(pairs);
+            browser.Quit();
 
-            var response = client.PostAsync(@"https://npm.by/booking/route-time", content).Result;
+            //var httpClientHandler = new HttpClientHandler()
+            //{
+            //    Credentials = new NetworkCredential("296874243", "geneva2788", "npm.by"),
+            //};
+
+            //var client = new HttpClient(httpClientHandler)
+            //{
+            //    BaseAddress = new Uri(@"https://npm.by/"),
+            //};
+
+            ////id_departure_station: 36
+            ////departure_is_waypoint: 0
+            ////id_arrival_station: 63
+            ////arrival_is_waypoint: 0
+            ////date: 06 - 01 - 2018
+
+            //var pairs = new List<KeyValuePair<string, string>>
+            //{
+            //    new KeyValuePair<string, string>( "id_departure_station", "36" ),
+            //    new KeyValuePair<string, string>( "departure_is_waypoint", "0"  ),
+            //    new KeyValuePair<string, string> ( "id_arrival_station", "63"  ),
+            //    new KeyValuePair<string, string> ( "arrival_is_waypoint","0" ),
+            //    new KeyValuePair<string, string> ( "date", "06-01-2018" )
+            //};
+            //var content = new FormUrlEncodedContent(pairs);
+
+            //var response = client.PostAsync(@"https://npm.by/booking/route-time", content).Result;
 
 
             //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "Your Oauth token");
